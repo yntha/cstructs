7c2725627678bdc9007a87fec2b15027ea174a1c
yntha <126660548+yntha@users.noreply.github.com>
2023-12-01 12:26:09 -0500

    Remove InvalidFormatString

--- src/cstructs/exc.py
+++ src/cstructs/exc.py
@@ -13,9 +13,5 @@
 #  You should have received a copy of the GNU General Public License along with        -
 #  this program. If not, see <http://www.gnu.org/licenses/>.                           -
 # --------------------------------------------------------------------------------------
-class InvalidFormatString(Exception):
-    pass
-
-
 class InvalidByteOrder(Exception):
     pass
