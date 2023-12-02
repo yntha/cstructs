d9917c56c5538126583868309a0fcd981d6d828d
yntha <126660548+yntha@users.noreply.github.com>
2023-12-01 19:18:50 -0500

    Add NativeTypes This class represents the primitive types seen in languages like C, C++, or Rust. Each type is an instance of `NativeType`, which is a container class that has the following fields: * `name`: typedef name * `size`: the size of this typedef in bytes * `python_type`: the transliterated type of this typedef in python * `repeat_length`: the number of times this typedef is sequentially repeated in a piece of data
    `repeat_length` receives its value when the user calls the NativeType with a number as its first parameter. Ex:
    ```
    @datastruct
    class MyStruct(metaclass=DataStruct):
        a: NativeTypes.uint32(12)
    ```
    will invoke the NativeType's `__call__` function and set the `repeat_length` field to 12, signifying that this field occupies 12 uint32's in the data. The size of this field in the metadata would thus be `NativeTypes.uint32.size * 12`.
    
    Additionally, the `bytestring` NativeType will initially have a size of -1. Byte-strings are variable in length, so their sizes are set by the `repeat_length` field above. So, for example:
    ```
    @datastruct
    class MyStruct(metaclass=DataStruct):
        a: NativeTypes.bytestring(8)
    ```
    will produce a field whose metadata `size` is 8, and whose value would be a `bytes` object of 8 bytes.

---
+++ src/cstructs/nativetypes.py
@@ -0,0 +1,75 @@
+# --------------------------------------------------------------------------------------
+#  Copyright(C) 2023 yntha                                                             -
+#                                                                                      -
+#  This program is free software: you can redistribute it and/or modify it under       -
+#  the terms of the GNU General Public License as published by the Free Software       -
+#  Foundation, either version 3 of the License, or (at your option) any later          -
+#  version.                                                                            -
+#                                                                                      -
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY     -
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A     -
+#  PARTICULAR PURPOSE. See the GNU General Public License for more details.            -
+#                                                                                      -
+#  You should have received a copy of the GNU General Public License along with        -
+#  this program. If not, see <http://www.gnu.org/licenses/>.                           -
+# --------------------------------------------------------------------------------------
+import enum
+
+
+class NativeType:
+    def __init__(self, name: str, size: int, python_type: type):
+        self.name = name
+        self.size = size
+        self.python_type = python_type
+
+        # repeat length is set in the __call__ method
+        self.repeat_length = 1
+
+    # this function allows for the user to specify the repeat length
+    # of a native type by calling the NativeType instance. Ex:
+    # NativeType.uint64(5) will invoke this function and set the repeat
+    # length to 5.
+    def __call__(self, repeat_length):
+        self.repeat_length = repeat_length
+
+        if self.size == -1:
+            self.size = repeat_length
+
+        return self
+
+    def __repr__(self):
+        return f"NativeType({self.name}, {self.size})"
+
+    def __str__(self):
+        return self.name
+
+
+class NativeTypes(enum.Enum):
+    # long typedef names
+    uint64 = NativeType("uint64", 8, int)
+    uint32 = NativeType("uint32", 4, int)
+    uint16 = NativeType("uint16", 2, int)
+    uint8 = NativeType("uint8", 1, int)
+    int64 = NativeType("int64", 8, int)
+    int32 = NativeType("int32", 4, int)
+    int16 = NativeType("int16", 2, int)
+    int8 = NativeType("int8", 1, int)
+    double = NativeType("double", 8, float)
+    float = NativeType("float", 4, float)
+    char = NativeType("char", 1, str)
+    bool = NativeType("bool", 1, bool)
+
+    # bytestring is a special case, it's size is variable
+    # and is specified by the user. The size is set in the
+    # __call__ method of the NativeType class.
+    bytestring = NativeType("bytestring", -1, bytes)
+
+    # short typedef names
+    u64 = uint64
+    u32 = uint32
+    u16 = uint16
+    u8 = uint8
+    i64 = int64
+    i32 = int32
+    i16 = int16
+    i8 = int8
