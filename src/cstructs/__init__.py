21175aec5ac92de9f2c3b70beedea82d50d5cb42
yntha <126660548+yntha@users.noreply.github.com>
2023-12-02 12:10:03 -0500

    Import NativeTypes to the frontend for easier access

--- src/cstructs/__init__.py
+++ src/cstructs/__init__.py
@@ -14,6 +14,7 @@
 #  this program. If not, see <http://www.gnu.org/licenses/>.                           -
 # --------------------------------------------------------------------------------------
 from cstructs.datastruct import datastruct as backend_datastruct, DataStruct
+from cstructs.nativetypes import NativeTypes
 
 
 def datastruct(*args, **kwargs):
